07421b3c39bb2baaf174ca7a2c963e510252445c
deepak10100 <deepakmalakar98185@gmail.com>
2025-05-26 19:13:33 +0530

    update 0-4

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ export default function Navbar() {
     <header className=' bg-amber-600 text-amber-50 p-5 w-full'>
         <nav>
             <ul className='md:flex hidden gap-5 items-center'>
-                <li><NavLink to={'/'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':""}>Home</NavLink></li>
-                <li><NavLink to={'/ecommerce'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':""}>Ecommerce</NavLink></li>
-                <li><NavLink to={'/form'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':""}>Form</NavLink></li>
-                <li><NavLink to={'/todo'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':""}>Todo Redux-toolkit</NavLink></li>
+                <li><NavLink to={'/'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':" hover:bg-amber-200 text-white hover:text-black p-2  rounded-sm font-medium"}>Home</NavLink></li>
+                <li><NavLink to={'/ecommerce'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':"hover:bg-amber-200 text-white hover:text-black  p-2  rounded-sm font-medium"}>Ecommerce</NavLink></li>
+                <li><NavLink to={'/form'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':"hover:bg-amber-200 text-white hover:text-black p-2  rounded-sm font-medium"}>Form</NavLink></li>
+                <li><NavLink to={'/todo'}  className={({isActive}) =>isActive?' bg-amber-200 text-black p-2  rounded-sm font-medium':"hover:bg-amber-200 text-white hover:text-black p-2  rounded-sm font-medium"}>Todo Redux-toolkit</NavLink></li>
             </ul>
         </nav>
     </header>
